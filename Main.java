1791d2b498b590a0a54d312392b25b6a623151ea
aashikh-niju <149099113+aashikh-niju@users.noreply.github.com>
2025-01-12 20:40:59 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main{
+    public static void main(String[] args){
+        int a,b,c,temp;
+        System.out.println("before swapping........");
+        a=10;
+        b=20;
+        c=30;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        temp=a;
+        a=b;
+        b=c;
+        c=temp;
+        System.out.println("after swapping........");
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+
+    }
+
+}
